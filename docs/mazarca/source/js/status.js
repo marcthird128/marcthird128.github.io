f7c23b364c0cad94d6178f06bb8371c2c63db68a
rumyantsev168 <Rmisha08len@yandex.ru>
2025-10-26 01:15:21 +0300

    improved recipes page (still not finished though); new status-fetching script; changed titles to be unique for each page

--- docs/mazarca/source/js/status.js
+++ docs/mazarca/source/js/status.js
@@ -1,9 +1,6 @@
 const timer = document.getElementById("timer");
-const reportBtn = document.getElementById("report-offline");
-let link = "";
 timer.innerText = "Fetching data... Please wait";
 
-var newDate = new Date();
 Date.prototype.today = function() {
     return ((this.getDate() < 10)?"0":"") + this.getDate() + "/" + (((this.getMonth()+1) < 10)?"0":"") + (this.getMonth()+1) + "/" + this.getFullYear();
 };
@@ -11,34 +8,36 @@ Date.prototype.timeNow = function() {
     return ((this.getHours() < 10)?"0":"") + this.getHours() + ":" + ((this.getMinutes() < 10)?"0":"") + this.getMinutes() + ":" + ((this.getSeconds() < 10)?"0":"") + this.getSeconds();
 };
 
-function getPing(address, output) {
+function getPing(host, output) {
     output.style.color = "gray";
     setInterval(async () => {
-        let online = await async function() {
+        let online = await (async () => {
             let cache_preventer = btoa(Date.now() + "_" + Math.random());
-            let url = address + "?" + cache_preventer;
-            let resolve;
-            fetch(url).then((v) => { resolve(true) }, (v) => { resolve(false) });
-            return new Promise((res) => { resolve = res });
-        };
+            let url = "http://" + host + ".maza64.xyz" + "?" + cache_preventer;
+            try {
+                const response = await fetch(url);
+                if (response.status === 401 || (response.status >= 200 && response.status < 300)) { return true }
+                return false;
+            } catch (error) { return false }
+        })();
         var newDate = new Date();
-        currentTime = ((newDate.getHours() < 10)?"0":"") + newDate.getHours() + ":" + ((newDate.getMinutes() < 10)?"0":"") + newDate.getMinutes() + ":" + ((newDate.getSeconds() < 10)?"0":"") + newDate.getSeconds();
+        currentTime = newDate.timeNow();
         if (online) {
-            console.log(`[Status Check ${currentTime}] ${address} is up`);
+            console.log(`[Status Check ${currentTime}] ${host} is up`);
             output.style.color = "#00AA00";
             output.style.textShadow = "0px 0px 10px #00AA0060";
             output.innerText = "Online";
-            reportBtn.style.display = "none";
+            document.getElementById(host+"-report-offline").style.display = "none";
         } else {
-            console.log(`[Status Check ${currentTime}] ${address} is down`);
+            console.log(`[Status Check ${currentTime}] ${host} is down`);
             output.style.color = "#AA0000";
             output.style.textShadow = "0px 0px 10px #AA000060";
             output.innerText = "Offline";
-            reportBtn.style.display = "block";
+            document.getElementById(host+"-report-offline").style.display = "block";
         };
         timer.innerText = `Checking every 3 seconds. Last checked at ${currentTime}`;
     }, 3000);
 };
 
-getPing("https://eagler.maza64.xyz", document.getElementById("mazarca-status"));
-getPing("https://www.eaglercraft.com", document.getElementById("eagler-status"));
+getPing("pi", document.getElementById("pi-status"));
+getPing("oldlaptop", document.getElementById("oldlaptop-status"));
