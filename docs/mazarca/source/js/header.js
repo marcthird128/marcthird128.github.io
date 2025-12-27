5c00081a19ebb4b74b677b5a1de2cbcc44527df4
rumyantsev168 <Rmisha08len@yandex.ru>
2025-12-28 00:13:45 +0300

    improved snow effect now can be turned off

--- docs/mazarca/source/js/header.js
+++ docs/mazarca/source/js/header.js
@@ -1,12 +1,53 @@
-// adds header to the page
+// Adds header to the page
+let date = new Date();
+let isChristmas = (date.getMonth() === 0 || date.getMonth() === 1 || date.getMonth() === 11) ? true : false
 
-const defaultHeader = '<nav><div id="nav-btn"><svg xmlns="http://www.w3.org/2000/svg" width="30px" height="30px" viewBox="0 0 1024 1024"><path fill="white" d="M904 160H120c-4.4 0-8 3.6-8 8v64c0 4.4 3.6 8 8 8h784c4.4 0 8-3.6 8-8v-64c0-4.4-3.6-8-8-8m0 624H120c-4.4 0-8 3.6-8 8v64c0 4.4 3.6 8 8 8h784c4.4 0 8-3.6 8-8v-64c0-4.4-3.6-8-8-8m0-312H120c-4.4 0-8 3.6-8 8v64c0 4.4 3.6 8 8 8h784c4.4 0 8-3.6 8-8v-64c0-4.4-3.6-8-8-8"/></svg></div></nav><div><a href="https://maza64.xyz"><img class="logo" src="source/cookie.png" height="50px"></a></div><div><a href="https://maza64.xyz"><img src="source/logo.png" height="35px"></a><p class="splash" id="splash-text"></p></div><div style="flex-grow:1"></div><div><a href="https://discord.com/invite/R6nMUHe9DK" target="_blank"><img class="discord-social" src="source/discord.jpg" width="35px"></a></div><div><a href="https://reddit.com/r/MazarcaSMP" target="_blank"><img class="reddit-social" src="source/reddit.jpg" height="35px"></a></div>';
+const defaultHeader = `
+    <nav>
+        <div id="nav-btn">
+            <svg xmlns="http://www.w3.org/2000/svg" width="30px" height="30px" viewBox="0 0 1024 1024">
+                <path fill="white" d="M904 160H120c-4.4 0-8 3.6-8 8v64c0 4.4 3.6 8 8 8h784c4.4 0 8-3.6 8-8v-64c0-4.4-3.6-8-8-8m0 624H120c-4.4 0-8 3.6-8 8v64c0 4.4 3.6 8 8 8h784c4.4 0 8-3.6 8-8v-64c0-4.4-3.6-8-8-8m0-312H120c-4.4 0-8 3.6-8 8v64c0 4.4 3.6 8 8 8h784c4.4 0 8-3.6 8-8v-64c0-4.4-3.6-8-8-8"/>
+            </svg>
+        </div>
+    </nav>
+    <div>
+        <a href="https://maza64.xyz">
+            <img class="logo" src="source/cookie.png" height="50px">
+        </a>
+    </div>
+    <div>
+        <a href="https://maza64.xyz">
+            <img src="source/logo.png" height="35px">
+        </a>
+        <p class="splash" id="splash-text"></p>
+    </div>
+    <div style="flex-grow:1"></div>
+    ${isChristmas ? `<div>
+        <label class="checkbox-container">
+            <input type="checkbox" id="snow-checkbox" checked>
+            <div style="display: flex; gap: 3px;">
+                <span class="checkmark"></span>
+                <small>Snow</small>
+            </div>
+        </label>
+    </div>` : ""}
+    <div>
+        <a href="https://discord.com/invite/R6nMUHe9DK" target="_blank">
+            <img class="discord-social" src="source/discord.jpg" width="35px">
+        </a>
+    </div>
+    <div>
+        <a href="https://reddit.com/r/MazarcaSMP" target="_blank">
+            <img class="reddit-social" src="source/reddit.jpg" height="35px">
+        </a>
+    </div>
+`;
 const headerElement = document.createElement("header");
 headerElement.innerHTML = defaultHeader;
 document.getElementsByTagName("main")[0].before(headerElement);
 console.log("Header added successfully!");
 
-// fetch motd text
+// Fetch motd text
 fetch("source/data/splash.json")
 .then(obj => obj.text())
 .then(res = function(res) {
@@ -19,4 +60,32 @@ fetch("source/data/splash.json")
         console.log("Current splash text is", `'${parsed.values[index]}'`, "with index of", index);
         motdElement.innerText = parsed.values[index];
     } else { console.log("Splash texts are disabled") };
+});
+
+// Snow toggle
+document.addEventListener("DOMContentLoaded", () => {
+    if (isChristmas) {
+        const snowCheckbox = document.getElementById("snow-checkbox");
+        const snowCanvas = document.getElementById("snow-canvas");
+        const savedSnowState = sessionStorage.getItem("snowEnabled");
+        if (savedSnowState !== null) {
+            const isChecked = savedSnowState === "true";
+            snowCheckbox.checked = isChecked;
+            if (snowCanvas) {
+                snowCanvas.style.display = isChecked ? "block" : "none";
+            };
+        } else {
+            snowCheckbox.checked = true;
+            if (snowCanvas) {
+                snowCanvas.style.display = "block";
+            };
+        };
+        snowCheckbox.addEventListener("change", () => {
+            const isChecked = snowCheckbox.checked;
+            sessionStorage.setItem("snowEnabled", isChecked);
+            if (snowCanvas) {
+                snowCanvas.style.display = isChecked ? "block" : "none";
+            };
+        });
+    };
 });
--- docs/mazarca/source/js/snow.js
+++ docs/mazarca/source/js/snow.js
@@ -1,6 +1,13 @@
-let date = new Date();
-if (date.getMonth() == 0 || date.getMonth() == 1 || date.getMonth() == 11) {
+if (date.getMonth() === 0 || date.getMonth() === 1 || date.getMonth() === 11) {
+    const snowCheckbox = document.getElementById("snow-checkbox");
+    if (!snowCheckbox) {
+        console.warn("Snow checkbox not found. Skipping snow effect.");
+        exit();
+    };
+    snowCheckbox.parentElement.style.display = "block";
+
     const canvas = document.createElement("canvas");
+    canvas.id = "snow-canvas";
     canvas.style.position = "fixed";
     canvas.style.top = "0";
     canvas.style.left = "0";
@@ -11,6 +18,12 @@ if (date.getMonth() == 0 || date.getMonth() == 1 || date.getMonth() == 11) {
     const ctx = canvas.getContext("2d");
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
+
+    const savedState = sessionStorage.getItem("snowEnabled");
+    const isChecked = savedState === null ? true : savedState === "true";
+
+    snowCheckbox.checked = isChecked;
+    canvas.style.display = isChecked ? "block" : "none";
 
     class Snowflake {
         constructor() {
@@ -46,21 +59,50 @@ if (date.getMonth() == 0 || date.getMonth() == 1 || date.getMonth() == 11) {
         snowflakes.push(new Snowflake());
     };
 
+    let animationId = null;
+
     function animate() {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-        snowflakes.forEach(flake => {
+        snowflakes.forEach((flake) => {
             flake.update();
             flake.draw();
         });
 
-        requestAnimationFrame(animate);
+        animationId = requestAnimationFrame(animate);
     };
+
+    function startAnimation() {
+        if (!animationId) {
+            animationId = requestAnimationFrame(animate);
+        };
+    };
+
+    function stopAnimation() {
+        if (animationId) {
+            cancelAnimationFrame(animationId);
+            animationId = null;
+        };
+    };
+
+    if (isChecked) {
+        startAnimation();
+    };
+
+    snowCheckbox.addEventListener("change", () => {
+        const checked = snowCheckbox.checked;
+        sessionStorage.setItem("snowEnabled", checked);
+        if (checked) {
+            canvas.style.display = "block";
+            startAnimation();
+        } else {
+            canvas.style.display = "none";
+            stopAnimation();
+        };
+    });
 
     window.addEventListener("resize", () => {
         canvas.width = window.innerWidth;
         canvas.height = window.innerHeight;
     });
-
-    animate();
 };
